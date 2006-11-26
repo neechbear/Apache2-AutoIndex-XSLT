3122ec20dc899fc5a91e6abbff77b9f550bb47e7
Nicola Worthington <nicolaw@tfb.net>
2006-11-26 22:26:39 +0000

    git-svn-id: file:///var/tmp/data/svnroot/Apache2-AutoIndex-XSLT@822 610ec7d1-1c12-4784-ad90-13d12571cf29

--- lib/Apache2/AutoIndex/XSLT.pm
+++ lib/Apache2/AutoIndex/XSLT.pm
@@ -22,29 +22,142 @@
 package Apache2::AutoIndex::XSLT;
 # vim:ts=4:sw=4:tw=78
 
+use 5.6.1;
 use strict;
-no strict qw(subs);
-use vars qw($VERSION);
-#use mod_perl2;
+use warnings;
+no strict qw(subs); # This is for ./DistBuild.PL only - can be commented out
+use File::Spec qw();
 use Apache2::RequestRec qw();
-use Apache2::Const -compile => qw(DIR_MAGIC_TYPE DECLINED);
+use Apache2::Access qw();
+use Apache2::Log qw();
+use Apache2::Const -compile => qw(:common :options DIR_MAGIC_TYPE);
 
-$VERSION = '0.00' || sprintf('%d.%02d', q$Revision: 531 $ =~ /(\d+)/g);
+use vars qw($VERSION);
+$VERSION = '1.01' || sprintf('%d.%02d', q$Revision: 531 $ =~ /(\d+)/g);
+
 
 # Let's deal with this another time shall we?
 #if (mod_perl2->module('Apache::Status')){
-#	Apache::Status->menu_item('AutoIndex' => sprintf('%s status',__PACKAGE__), \&status);
+#	Apache::Status->menu_item('AutoIndex' => sprintf('%s status',__PACKAGE__), \&_status);
 #}
+
 
 sub handler {
 	my $r = shift;
-	return DECLINED unless $r->content_type && $r->content_type eq DIR_MAGIC_TYPE;
 
-	
+	# Only handle directories
+	return Apache2::Const::DECLINED unless $r->content_type &&
+			$r->content_type eq Apache2::Const::DIR_MAGIC_TYPE;
 
+	# Make sure we're at a URL with a trailing slash
+	unless ($r->uri =~ m,/$,) {# || $r->path_info){
+		$r->headers_out->add(Location => sprintf('%s/%s',
+				$r->uri,
+				($r->args ? '?'.$r->args : '')
+			));
+		return Apache2::Const::REDIRECT;
+	}
+
+	# Return a directory listing if we're allowed to
+	if ($r->allow_options & Apache2::Const::OPT_INDEXES) {
+		$r->content_type('text/xml');
+		return Apache2::Const::OK if $r->header_only;
+		return _dir_xml($r);
+
+	# Otherwise he's not the messiah, he's a very naughty boy
+	} else {
+		$r->log_reason(
+				sprintf('%s Directory index forbidden by rule', __PACKAGE__),
+				sprintf('%s (%s)', $r->uri, $r->filename),
+			);
+		return Apache2::Const::FORBIDDEN;
+	}
 }
 
-sub status {
+
+sub _dir_xml {
+	my $r = shift;
+
+	my $dh;
+	unless (opendir($dh,$r->filename)) {
+		$r->log_reason(
+				sprintf("%s Unable to open directory handle for '%s': %s",
+					__PACKAGE__, $r->filename, $!),
+				sprintf('%s (%s)', $r->uri, $r->filename),
+			);
+		return Apache2::Const::FORBIDDEN;
+	}
+
+	_print_xml_header();
+	print "<index>\n";
+	while (my $id = readdir($dh)) {
+		next if $id eq '.';
+		my $filename = File::Spec->catfile($r->filename,$id);
+
+		my $type = -d $filename && $id eq '..' ? 'updir' :
+					$id =~ /^My[ -_]?(Computer|Documents|
+						Pictures|Videos|Music)$/xi ? 'special' :
+					-l $filename ? 'link' :
+					-d $filename ? 'dir' :
+					'file';
+
+		my $stat = _stat_file($filename);
+		my $element = sprintf('<%s id="%s" %s />',
+				$type,
+				$id,
+				join(' ',map { sprintf('%s="%s"',$_,$stat->{$_}) }
+					keys(%{$stat})),
+			);
+
+		print "\t$element\n";
+	}
+	print "</index>\n";
+
+	closedir($dh);
+	return Apache2::Const::OK;
+}
+
+
+sub _stat_file {
+	my %stat;
+	@stat{qw(dev ino mode nlink uid gid rdev size
+			atime mtime ctime blksize blocks)} = stat($_[0]);
+	return \%stat;
+}
+
+
+sub _print_xml_header {
+	my $stylesheet = shift;
+
+	my @dtd = (
+			'<!DOCTYPE svn [',
+			'  <!ELEMENT svn   (index)>',
+			'  <!ATTLIST svn   version CDATA #REQUIRED',
+			'                  href    CDATA #REQUIRED>',
+			'  <!ELEMENT index (updir?, (file | dir)*)>',
+			'  <!ATTLIST index name    CDATA #IMPLIED',
+			'                  path    CDATA #IMPLIED',
+			'                  rev     CDATA #IMPLIED>',
+			'  <!ELEMENT updir EMPTY>',
+			'  <!ELEMENT file  EMPTY>',
+			'  <!ATTLIST file  name    CDATA #REQUIRED',
+			'                  href    CDATA #REQUIRED>',
+			'  <!ELEMENT dir   EMPTY>',
+			'',
+			'  <!ATTLIST dir   name    CDATA #REQUIRED',
+			'                  href    CDATA #REQUIRED>',
+			']>',
+		);
+	@dtd = ();
+
+	print qq{<?xml version="1.0"?>\n};
+	print qq{<?xml-stylesheet type="text/xsl" href="$stylesheet"?>}
+		if $stylesheet;
+	print "$_\n" for @dtd;
+}
+
+
+sub _status {
 	my $r = shift;
 
 	my @status;
@@ -57,6 +170,7 @@ sub status {
 
 	return \@status;
 }
+
 
 1;
 
