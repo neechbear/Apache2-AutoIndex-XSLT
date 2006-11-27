01b89c8ad675401ff2ce16f3bfea6dfc31c02775
Nicola Worthington <nicolaw@tfb.net>
2006-11-27 01:01:17 +0000

    git-svn-id: file:///var/tmp/data/svnroot/Apache2-AutoIndex-XSLT@824 610ec7d1-1c12-4784-ad90-13d12571cf29

--- lib/Apache2/AutoIndex/XSLT.pm
+++ lib/Apache2/AutoIndex/XSLT.pm
@@ -27,13 +27,15 @@ use strict;
 use warnings;
 no strict qw(subs); # This is for ./DistBuild.PL only - can be commented out
 use File::Spec qw();
+use Fcntl qw(:mode);
 use Apache2::RequestRec qw();
 use Apache2::Access qw();
 use Apache2::Log qw();
+use Apache2::URI qw();
 use Apache2::Const -compile => qw(:common :options DIR_MAGIC_TYPE);
 
 use vars qw($VERSION);
-$VERSION = '1.01' || sprintf('%d.%02d', q$Revision: 531 $ =~ /(\d+)/g);
+$VERSION = '0.01' || sprintf('%d.%02d', q$Revision: 531 $ =~ /(\d+)/g);
 
 
 # Let's deal with this another time shall we?
@@ -88,46 +90,98 @@ sub _dir_xml {
 		return Apache2::Const::FORBIDDEN;
 	}
 
-	_print_xml_header();
-	print "<index>\n";
+	my $xslt = -f File::Spec->catfile($r->document_root,'index.xslt')
+			? 'index.xslt' : '';
+	_print_xml_header($xslt);
+	printf "<index path=\"%s\" href=\"%s\" >\n", $r->uri, $r->construct_url;
+
 	while (my $id = readdir($dh)) {
 		next if $id eq '.';
+		next if $id eq '..' && $r->uri =~ m,^/?$,;
+
 		my $filename = File::Spec->catfile($r->filename,$id);
+		my $type = _file_type ($id,$filename);
+		my $attr = _stat_file($filename);
 
-		my $type = -d $filename && $id eq '..' ? 'updir' :
-					$id =~ /^My[ -_]?(Computer|Documents|
-						Pictures|Videos|Music)$/xi ? 'special' :
-					-l $filename ? 'link' :
-					-d $filename ? 'dir' :
-					'file';
+		($attr->{ext}) = $id =~ /\.([a-z0-9_]+)$/i;
+		if ($type eq 'file') {
+			$attr->{icon} = -f File::Spec->catfile($r->document_root,'icons',lc("$attr->{ext}.gif")) ?
+					'/icons/'.lc("$attr->{ext}.gif") : '/icons/__unknown.gif';
+		}
 
-		my $stat = _stat_file($filename);
-		my $element = sprintf('<%s id="%s" %s />',
-				$type,
-				$id,
-				join(' ',map { sprintf('%s="%s"',$_,$stat->{$_}) }
-					keys(%{$stat})),
+		$attr->{title} = $id;
+		$attr->{desc} = '';
+
+		my $element = sprintf('<%s id="%s" %s />', $type, $id,
+				join(' ',map { sprintf('%s="%s"',$_,$attr->{$_}) }
+					keys(%{$attr})),
 			);
 
 		print "\t$element\n";
 	}
-	print "</index>\n";
 
+	print "</index>\n";
 	closedir($dh);
 	return Apache2::Const::OK;
+}
+
+
+sub _file_type {
+	my ($id,$filename) = @_;
+	return -d $filename && $id eq '..' ? 'updir' :
+#		$id =~ /^(My[ -_]?(Computer|Documents|Pictures|Videos|Music)|
+#		Network[ -_]?Neighborhood|Logitech[ -_]?Webcam|Control[ -_]?Panel)
+#		$/xi ? 'special' :
+#			-l $filename ? 'link' :
+			-d $filename ? 'dir' :
+			'file';
 }
 
 
 sub _stat_file {
 	my %stat;
 	@stat{qw(dev ino mode nlink uid gid rdev size
-			atime mtime ctime blksize blocks)} = stat($_[0]);
-	return \%stat;
+			atime mtime ctime blksize blocks)} = lstat($_[0]);
+
+	my %rtn;
+	$rtn{$_} = $stat{$_} for qw(uid gid mtime ctime size);
+	$rtn{perms} = _file_mode($stat{mode});
+	$rtn{owner} = scalar getpwuid($rtn{uid});
+	$rtn{group} = scalar getgrgid($rtn{gid});
+
+	return \%rtn;
+}
+
+
+sub _file_mode {
+	my $mode = shift;
+	return (S_ISREG($mode)  ? '-' :
+			S_ISDIR($mode)  ? 'd' :
+			S_ISLNK($mode)  ? 'l' :
+			S_ISBLK($mode)  ? 'b' :
+			S_ISCHR($mode)  ? 'c' :
+			S_ISFIFO($mode) ? 'p' :
+			S_ISSOCK($mode) ? 's' : '?' ) .
+
+			( ($mode & S_IRUSR) ? 'r' : '-' ) .
+			( ($mode & S_IWUSR) ? 'w' : '-' ) .
+			( ($mode & S_ISUID) ? (($mode & S_IXUSR) ? 's' : 'S')
+								: (($mode & S_IXUSR) ? 'x' : '-') ) .
+
+			( ($mode & S_IRGRP) ? 'r' : '-' ) .
+			( ($mode & S_IWGRP) ? 'w' : '-' ) .
+			( ($mode & S_ISGID) ? (($mode & S_IXGRP) ? 's' : 'S')
+								: (($mode & S_IXGRP) ? 'x' : '-') ) .
+
+			( ($mode & S_IROTH) ? 'r' : '-' ) .
+			( ($mode & S_IWOTH) ? 'w' : '-' ) .
+			( ($mode & S_ISVTX) ? (($mode & S_IXOTH) ? 't' : 'T')
+								: (($mode & S_IXOTH) ? 'x' : '-') );
 }
 
 
 sub _print_xml_header {
-	my $stylesheet = shift;
+	my $xslt = shift;
 
 	my @dtd = (
 			'<!DOCTYPE svn [',
@@ -151,8 +205,7 @@ sub _print_xml_header {
 	@dtd = ();
 
 	print qq{<?xml version="1.0"?>\n};
-	print qq{<?xml-stylesheet type="text/xsl" href="$stylesheet"?>}
-		if $stylesheet;
+	print qq{<?xml-stylesheet type="text/xsl" href="$xslt"?>} if $xslt;
 	print "$_\n" for @dtd;
 }
 
@@ -182,7 +235,19 @@ Apache2::AutoIndex::XSLT - XSLT Based Directory Listings
 
 =head1 SYNOPSIS
 
+ <Location />
+     SetHandler perl-script
+     PerlResponseHandler Apache2::AutoIndex::XSLT
+     Options +Indexes
+ </Location>
+
 =head1 DESCRIPTION
+
+This module is designed as a drop in mod_perl2 replacement for the mod_dir and
+mod_index modules. It uses user configurable XSLT stylesheets to generate the
+directory listings.
+
+THIS IS A DEVELOPMENT RELEASE.
 
 =head1 SEE ALSO
 
@@ -202,6 +267,9 @@ If you like this software, why not show your appreciation by sending the
 author something nice from her
 L<Amazon wishlist|http://www.amazon.co.uk/gp/registry/1VZXC59ESWYK0?sort=priority>? 
 ( http://www.amazon.co.uk/gp/registry/1VZXC59ESWYK0?sort=priority )
+
+With special thanks to Jennifer Beattie for developing the example XSLT
+stylesheets.
 
 =head1 COPYRIGHT
 
