36cb225847a67a3add88478bb07811737a3571ad
Nicola Worthington <nicolaw@tfb.net>
2007-01-01 15:14:45 +0000

    git-svn-id: file:///var/tmp/data/svnroot/Apache2-AutoIndex-XSLT@882 610ec7d1-1c12-4784-ad90-13d12571cf29

--- lib/Apache2/AutoIndex/XSLT.pm
+++ lib/Apache2/AutoIndex/XSLT.pm
@@ -1004,12 +1004,12 @@ at all if the XSLT stylesheet used does not use them.
 
 =head2 RenderXSLTEnvVar
 
-     SetEnv RenderXSLT=1
-     BrowserMatch "Firefox/(2.0|1.5|1.0.[234567])" RenderXSLT=0
-     BrowserMatch "MSIE [67].0" RenderXSLT=0
-     BrowserMatch "Netscape/8" RenderXSLT=0
-     BrowserMatch "Opera/9" RenderXSLT=0
-     RenderXSLTEnvVar RenderXSLT
+     SetEnv RenderXSLT=On
+     BrowserMatch "Firefox/(2.0|1.5|1.0.[234567])" RenderXSLT=Off
+     BrowserMatch "MSIE [67].0" RenderXSLT=Off
+     BrowserMatch "Netscape/8" RenderXSLT=Off
+     BrowserMatch "Opera/9" RenderXSLT=Off
+     RenderXSLTEnvVar RenderXSLT 
 
 =head2 AddAlt
 
